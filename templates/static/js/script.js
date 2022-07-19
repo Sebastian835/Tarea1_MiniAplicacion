299629f486992634c37a6898f9609c8d2681a6cd
David Lopez <sebastian.830@outlook.com>
2022-07-19 17:04:33 -0500

    Prueba2

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -1,14 +1,39 @@
-$('#btn-validate').click(function() {
-    var $captcha = $('#recaptcha'),
-        response = grecaptcha.getResponse();
-    if (response.length === 0) {
-        $('.msg-error').text("reCAPTCHA is mandatory");
-        if (!$captcha.hasClass("error")) {
-            $captcha.addClass("error");
+$(function() {
+
+    $("form[name='validacion']").validate({
+        rules: {
+            nombre: {
+                required: true
+            },
+            apellido: {
+                required: true
+            },
+            edad: {
+                required: true,
+                min: 18
+            },
+            email: {
+                required: true,
+                email: true
+            },
+        },
+        messages: {
+            nombre: {
+                required: "Por favor, introduzca su nombre"
+            },
+            apellido: {
+                required: "Por favor, introduzca su apellido",
+            },
+            edad: {
+                required: "Por favor, introduzca su edad",
+                min: "mayor de edad"
+            },
+            email: {
+                required: "Por favor, introduce una dirección de correo",
+            },
+        },
+        submitHandler: function(form) {
+            form.submit();
         }
-    } else {
-        $('.msg-error').text('');
-        $captcha.removeClass("error");
-        alert('reCAPTCHA marked');
-    }
-})
+    });
+});
